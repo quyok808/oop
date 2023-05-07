083d47f35417b64bd9ed9c35513b0ea635796e3e
quyok808 <54305138+quyok808@users.noreply.github.com>
2023-05-07 21:17:16 +0700

    Add files via upload

--- introduce/create_pascal_triangle.java
+++ introduce/create_pascal_triangle.java
@@ -2,11 +2,9 @@ package introduce;
 
 public class create_pascal_triangle {
 	
-
-	
 	public static int[][] createPascalTriangle(int[] diagonalValues){
-		int[][] triangle_pascal = new int[5][];
 		int n = diagonalValues.length;
+		int[][] triangle_pascal = new int[n][];
 		for (int i = 0; i < n; i++) {
 			triangle_pascal[i] = new int[i+1];
 			triangle_pascal[i][i] = diagonalValues[i];
@@ -23,15 +21,16 @@ public class create_pascal_triangle {
 	
 	public static void Triangle(int[][] triangle) {
 		for (int i=0; i<triangle.length; i++) {
-			for (int j=0; j<triangle[i].length; j++) {
+			for (int j=0; j<=i; j++) {
 				System.out.print(triangle[i][j] + "\t");
 			}
 			System.out.println();
 		}
 	}	
 	public static void main(String[] args) {
-		int[][] triangle = new int[5][];
-		int[] diagonalValues = {1,5,2,0};
+		int[] diagonalValues = {1,5,2,0,7,8};
+		int n = diagonalValues.length;
+		int[][] triangle = new int[n][];
 		triangle = createPascalTriangle(diagonalValues);
 		Triangle(triangle);
 	}
